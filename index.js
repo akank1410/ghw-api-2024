6c7d1c80a044a94cb578f0e378953e517acbd47c
akank1410 <54724406+akank1410@users.noreply.github.com>
2024-04-14 22:28:39 +0530

    runnable code in index.js

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ app.get('/', async (req, res) =>{
     res.json({message : 'Hello GHW! Welcome to our stream'})
 })
 
+/* we are typing to take an api service
+in which if someone hits your api service they 
+get the list of all ghw challenges that are running */
 app.get('/challenges', async(req, res) => {
     
     axios.get('https://ghw.mlh.io/challenges')
     .then(response => {
         const html = response.data
+        //cheerio will help us reference all the html tags
+        //it'll take all the html data
         const $ = cheerio.load(html)
 
-       $('a.contains("")',html).each(function(){
+       $('a:contains("")',html).each(function(){
         const title = $(this).text()
         const url = $(this).attr('href')
 
@@ -25,9 +30,9 @@ app.get('/challenges', async(req, res) => {
             title,
             url
         })
-       }).catch(err=> console.log(err))
+       })
         res.json(challenges)
-    })
+    }).catch(err=> console.log(err))
 })
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
